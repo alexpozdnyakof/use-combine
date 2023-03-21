50481e5cb64f699701e8edc7f5827d3fa6df25ad
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-21 19:30:32 +0700

    refactor: extract types

--- index.ts
+++ index.ts
@@ -1,22 +1,19 @@
 import { Dispatch, Reducer, ReducerAction, ReducerState } from 'react';
 
+type Reducers<T, A> = {
+  [K in keyof T]: [
+    ReducerState<Reducer<T[K], A>>,
+    Dispatch<ReducerAction<Reducer<T[K], A>>>,
+  ];
+}
+
 export default function useCombine<
   State extends Record<string, object>,
   Action = any,
->(reducers: {
-  [K in keyof State]: [
-    ReducerState<Reducer<State[K], Action>>,
-    Dispatch<ReducerAction<Reducer<State[K], Action>>>,
-  ];
-}) {
-  const combinedState = Object.entries(reducers).reduce(
-    (acc, [sliceName, [subState]]) =>
-      Object.assign(acc, { [sliceName]: subState }),
-    {},
-  );
+>(reducers: Reducers<State, Action>) {
+  const combinedState = Object.entries(reducers).reduce((acc, [sliceName, [subState]]) => Object.assign(acc, { [sliceName]: subState }), {});
 
-  const combinedDispatch = (action: Action) =>
-    Object.values(reducers)
+  const combinedDispatch = (action: Action) => Object.values(reducers)
       .map(([_, dispatch]) => dispatch)
       .forEach((dispatch) => dispatch(action));
 
