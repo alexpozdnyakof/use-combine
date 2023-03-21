57a1dc131c26e8692fd0cbe83c361d61d144ae17
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-21 19:05:07 +0700

    add type testing

--- index.spec.ts
+++ index.spec.ts
@@ -1,36 +1,48 @@
 import useCombine from './index';
-import { vi, assert, expect, test } from 'vitest'
+import { vi, assert, expect, test,  expectTypeOf } from 'vitest'
 
 
-  const userInitialState = { name: 'name' };
-  const todosInitialState = { todos: [1, 2, 3] };
+const userInitialState = { name: 'name' };
+const todosInitialState = { todos: [1, 2, 3] };
 
-  test('should merge states with provided keys', () => {
-    const [state] = useCombine({
-      user: [userInitialState, (state) => state],
-      todos: [todosInitialState, (state) => state],
-    });
-
-    assert.deepEqual(state, {
-      user: userInitialState,
-      todos: todosInitialState,
-    });
+test('should merge states with provided keys', () => {
+  const [state] = useCombine({
+    user: [userInitialState, (state) => state],
+    todos: [todosInitialState, (state) => state],
   });
 
-  test('should call all passed reducers', () => {
-    const reducerA = vi.fn();
-    const reducerB = vi.fn();
-    const action = { type: 'init' };
-
-    const [_, dispatch] = useCombine({
-      user: [userInitialState, reducerA],
-      todos: [todosInitialState, reducerB],
-    });
-
-    dispatch(action);
-
-    expect(reducerA).toHaveBeenCalledTimes(1);
-    expect(reducerA).toHaveBeenCalledWith(action);
-    expect(reducerB).toHaveBeenCalledTimes(1);
-    expect(reducerB).toHaveBeenCalledWith(action);
+  assert.deepEqual(state, {
+    user: userInitialState,
+    todos: todosInitialState,
   });
+});
+
+test('should infer state type', () => {
+  const [state] = useCombine({
+    user: [userInitialState, (state) => state],
+    todos: [todosInitialState, (state) => state],
+  });
+
+  expectTypeOf(state).toEqualTypeOf<{
+    user: {name: string}
+    todos: {todos: number[]}
+  }>()
+})
+
+test('should call all passed reducers', () => {
+  const reducerA = vi.fn();
+  const reducerB = vi.fn();
+  const action = { type: 'init' };
+
+  const [_, dispatch] = useCombine({
+    user: [userInitialState, reducerA],
+    todos: [todosInitialState, reducerB],
+  });
+
+  dispatch(action);
+
+  expect(reducerA).toHaveBeenCalledTimes(1);
+  expect(reducerA).toHaveBeenCalledWith(action);
+  expect(reducerB).toHaveBeenCalledTimes(1);
+  expect(reducerB).toHaveBeenCalledWith(action);
+});
